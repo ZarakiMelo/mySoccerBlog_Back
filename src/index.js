cc4d77d7539cf79b11223b4ca1d116eea2fbfd60
ZarakiMelo <chahbisamy@hotmail.fr>
2025-02-21 14:50:59 +0100

    cors without /

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://my-soccer-blog-front.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://my-soccer-blog-front.vercel.app'],
   credentials: true
 })); 
