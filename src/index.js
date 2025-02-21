6ddecff5bdb0c47c40aa32c3231ced1c4d1a51e6
ZarakiMelo <chahbisamy@hotmail.fr>
2025-02-21 15:11:58 +0100

    logs to debug deploiement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-
+app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:5173',
@@ -15,4 +16,23 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   maxAge: 86400 
-})); 
+}));
+
+
+const routes = require('./routers');
+app.use(routes);
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something broke!' });
+});
+
+module.exports = app;
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.APP_PORT || 5050;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} 
