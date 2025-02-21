82c213bdd14257d6623c48f1cb9c4b2dc93402dc
ZarakiMelo <chahbisamy@hotmail.fr>
2025-02-21 15:08:54 +0100

    cors in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://my-soccer-blog-front.vercel.app'],
-  credentials: true
+  origin: [
+    'http://localhost:5173',
+    'https://my-soccer-blog-front.vercel.app',
+    'https://my-soccer-blog-front-git-main-zarakimelo.vercel.app',
+    'https://my-soccer-blog-front-zarakimelo.vercel.app' 
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 86400 
 })); 
